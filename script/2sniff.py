cbce7693c8293eea01adb012521d6138a0f448dd
Lei Shao <stefan9149@gmail.com>
2015-02-11 17:30:07 -0500

    Added New Version, Captured Encrption Methods

--- script/2sniff.py
+++ script/2sniff.py
@@ -14,6 +14,8 @@ def GetSSID(pkts):
 			mac = pkt.addr2  # MAC[Dot11] 
 			bssid = pkt[Dot11].addr3
 			capability = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}{Dot11ProbeResp:%Dot11ProbeResp.cap%}")
+			crypto = set()
+			ssid, channel = None, None
 		except AttributeError:
 			pass
 		if pkt.haslayer(Dot11Elt):
@@ -32,33 +34,29 @@ def GetSSID(pkts):
 					while Dot11Elt in layer:
 						if layer.ID == 3 and len(layer.info) == 1:
 							channel = int( ord(layer.info))
-						if layer.ID > 3:
-							break
+						if layer.ID == 48:
+							crypto.add("WPA2")
+						if layer.ID == 221 and layer.info.startswith('\x00P\xf2\x01\x01\x00'):
+							crypto.add("WPA")
 						layer = layer.payload
-					probe = {"Packet No.": index, "SSID": ssid, "BSSID": bssid, "Capability":capability, "Channel": channel}
+					if not crypto:
+						if 'privacy' in capability:
+							crypto.add("WEP")
+						else:
+							crypto.add("OPEN")
+					probe = {"Packet No.": index, "SSID": ssid, "BSSID": bssid, "Capability":capability, "Channel": channel, "Encryption": ' / '.join(crypto)}
 					key = "%s_%s" % (ssid, bssid)
 					unique_ap[key] = probe
-			#elif pkt.ID == 3: #ID = DSset (channel)
-				#print index
-			#	if len(pkt.info) == 1: 
-			#		channel = int( ord(pkt.info))
-			#		unique_ap[key].update({"Channel": channel})
-				#print index
-			#elif pkt.ID > 3:
-			#	unique_ap[key].update({"Channel": 0})
-			#	break
-       	     	#	pkt = pkt.payload
-			#layerCount += 1
 		index += 1
 
 GetSSID(pkts)
 
 od = collections.OrderedDict(sorted(unique_ap.items()))
 
-with open('output.txt', 'w') as out:
-	out.write(",".join(["Packet No.", "SSID", "BSSID(MAC)", "Capability", "Channel"]) + "\r\n")
+with open('output2.txt', 'w') as out:
+	out.write(",".join(["Packet No.", "SSID", "BSSID(MAC)", "Capability", "Channel", "Encryption"]) + "\r\n")
 	for key in od:
-	        out.write(",".join(['"%s"' % x for x in [od[key]['Packet No.'], od[key]['SSID'], od[key]['BSSID'], od[key]['Capability'],od[key]['Channel']]]) + "\r\n")
+	        out.write(",".join(['"%s"' % x for x in [od[key]['Packet No.'], od[key]['SSID'], od[key]['BSSID'], od[key]['Capability'],od[key]['Channel'], od[key]['Encryption']]]) + "\r\n")
 
 
 
